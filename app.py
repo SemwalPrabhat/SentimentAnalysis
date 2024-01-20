1a56443578910ecd7d22e2429c077a067cb34677
SemwalPrabhat <123309960+SemwalPrabhat@users.noreply.github.com>
2024-01-20 17:52:37 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,26 @@
+import streamlit as st
+import pickle
+import time
+import numpy as np
+
+# load the model
+model = pickle.load(open('pipe1.pkl', 'rb'))
+
+st.title('Twitter Sentiment Analysis')
+
+tweet = st.text_input('Enter your tweet')
+
+submit = st.button('Predict')
+
+if submit:
+    start = time.time()
+    prediction = model.predict([tweet])
+    end = time.time()
+    st.write('Prediction time taken: ', round(end-start, 2), 'seconds')
+    if(prediction[0]==0):
+        "Negative"
+    if(prediction[0]==1):
+        "Neutral"
+    if(prediction[0]==2):
+        "Positive"
+    st.write(prediction[0])
